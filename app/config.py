c546607b6203b4c9f07fb086d624e8a0285e5e91
Matias Schroder <matischroder99@gmail.com>
2024-06-18 22:54:13 +0930

    add env

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Config:
     VAPI_API_KEY = os.getenv("VAPI_API_KEY")
+    VAPI_PUBLIC_KEY = os.getenv("VAPI_PUBLIC_KEY")
     # EXTERNAL_API_URL = os.getenv("EXTERNAL_API_URL")
     # EXTERNAL_API_KEY = os.getenv("EXTERNAL_API_KEY")
     PORT = os.getenv("PORT")
