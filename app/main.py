a998c2da66af82bafdc954b8dd11f3afd4ad94a1
Matias Schroder <matischroder99@gmail.com>
2024-06-30 20:01:17 +0930

    add new function and cron

--- app/main.py
+++ app/main.py
@@ -3,15 +3,35 @@ import json
 from firebase_admin import credentials, initialize_app
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.background import BackgroundScheduler
+from contextlib import asynccontextmanager
 
-from app.routes import assistant, call, phone_number
+from app.routes import assistant, call, phone_number, function_calls
 
 
 firebase_credentials_path = "app/firebase/serviceAccountKey.json"
 cred = credentials.Certificate(firebase_credentials_path)
 initialize_app(cred)
 
-app = FastAPI()
+calls = {}
+
+
+def run_calls():
+    print("run calls")
+    # obtener campañas
+    # ver cuales tiene que correr un llamado y ver si es a esta hora o no
+    #
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(run_calls, "interval", minutes=60)
+    scheduler.start()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 cors_origin = os.environ.get("CORS_ORIGIN")
 if cors_origin is not None:
@@ -34,6 +54,10 @@ router.include_router(call.router, prefix="/call", tags=["Calls"])
 router.include_router(
     phone_number.router, prefix="/phone_number", tags=["Phone Numbers"]
 )
+router.include_router(
+    function_calls.router, prefix="/function_call", tags=["Function Call"]
+)
+
 
 app.include_router(router)
 
